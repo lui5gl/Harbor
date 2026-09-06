d8e3b6786406515110e8ee27f008a0744c1f06e2
lui5gl <lui5gl@outlook.com>
2026-09-06 17:36:05 -0600

    refactor: remove root route redirect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,3 @@
-<script lang="ts">
-  import { goto } from "$app/navigation";
-  import { onMount } from "svelte";
-
-  onMount(() => {
-    void goto("/services", { replaceState: true });
-  });
-</script>
-
 <svelte:head>
   <title>Harbor</title>
   <meta name="description" content="Harbor local development environment manager" />
